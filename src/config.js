ea876aaea2df1503b1f84c9110d18602c332d819
Dan Bartlett <danbart909@gmail.com>
2020-02-28 19:59:51 -0500

    .

--- src/config.js
+++ src/config.js
@@ -2,9 +2,6 @@ module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
   API_TOKEN: process.env.API_TOKEN || 0000,
-  DATABASE_URL: 'postgresql://postgres:401923@localhost/noteful',
-  TEST_DATABASE_URL: 'postgresql://postgres:401923@localhost/noteful-test'
+  DATABASE_URL: process.env.DATABASE_URL || 'postgresql://postgres:401923@localhost/noteful',
+  TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://postgres:401923@localhost/noteful-test'
 }
-
-// DATABASE_URL: process.env.DATABASE.URL || 'postgresql:postgres:401923@localhost/noteful',
-// TEST_DATABASE_URL: process.env.TEST.DATABASE.URL || 'postgresql:postgres:401923@localhost/noteful-test'
